1d6f23ac5cf23d558165fa54d1f9d18650b6d47e
muswain <mrutyunjay.swain@gmail.com>
2024-10-07 14:53:47 +0800

    feat: add pandas to_datetime

--- app.py
+++ app.py
@@ -2,7 +2,15 @@ import pandas as pd
 
 df = pd.read_csv('data.csv')
 
-df["name"].fillna('not-available', inplace = True) 
-df["age"].fillna(25, inplace = True) 
+df.fillna({'name': 'not-avaialble'}, inplace = True) 
+df.fillna({'age': 25}, inplace = True) 
+df.fillna({'joining_dt': "2019/05/07"}, inplace = True) 
+
+#df['joining_dt'] = pd.to_datetime(df['joining_dt'], 'coerce', format='yyyymmdd')
+
+age_mean = df['age'].mean();
+age_median = df['age'].median()
 
 print(df.to_string()) 
+
+print(f'mean age  is {age_mean} and median age is {age_median}') 
